c459015adda4278a1003fa6ba33ef486d35aa624
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-20 03:51:16 -0400

    added checkbox

--- onboard/src/App.js
+++ onboard/src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
+
+import { v4 as uuid } from 'uuid'
 import './App.css';
+import Form from './Form'
+import Friend from './Friend'
+
+
+
+const initialFormValues = {
+  username: '',
+  email:'',
+  role:'',
+  civil:'',
+  hobbies:{
+    hiking:false,
+    reading:false,
+    coding:false,
+  }
+}
 
 function App() {
+  const [friends,  setFriends] = useState([])
+  
+  const [formValues,setFormValue] = useState(initialFormValues)
+
+  const checkboxChange = (name,isChecked) => {
+    setFormValue({
+      ...formValues,hobbies: {
+        ...formValues.hobbies,
+        [name]: isChecked
+      }
+    })
+  }
+  const updateForm =(inputName, inputValue) => {
+    setFormValue({...formValues,[inputName]:inputValue})
+  }
+
+  const submitForm =() => {
+    const newFriend = {
+      id: uuid(),
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+      civil: formValues.civil,
+      hobbies:Object.keys(formValues.hobbies).filter(hob => formValues.hobbies[hob]),
+    }
+    if (!newFriend.username || !newFriend.email) return
+
+    setFriends([...friends,newFriend])
+
+    return setFormValue(initialFormValues)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <header><h1>Friend App</h1></header>
+
+        <Form 
+        
+        values={formValues}
+        update={ updateForm } 
+        submit={submitForm}
+        onboxChange={checkboxChange}
+        
+        />
+      {
+        friends.map(friend => {
+            
+              return <Friend detail={friend}/>
+            
+        })
+      }
+        
+
+
+
+
+
     </div>
-  );
+
+
+
+  )
 }
 
 export default App;
---
+++ onboard/src/Form.js
@@ -0,0 +1,117 @@
+import React from 'react'
+
+
+export default function Form(props) {
+    const {values, update,submit,onboxChange } = props
+
+    const onChange = evt => {
+        const {name, value} = evt.target
+       update(name, value)
+    }
+
+    const onCheckboxChange = evt => {
+        const { name, checked } = evt.target
+        onboxChange(name, checked)
+      }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    return (
+        <form className='form div' onSubmit={onSubmit}> 
+            <div>
+                <h2>Add a friend</h2>
+            </div>
+            <div>
+                <h4>General information</h4>
+
+                <label>Username:&nbsp;
+                    <input
+                        value={values.username}
+                        onChange={onChange}
+                        name='username'
+                        placeholder='type yourname'
+                        maxLength='20'
+                        type="text"
+                    />            
+                </label>
+
+                <label>Email:&nbsp;
+                    <input
+                    value={values.email}
+                    onChange={onChange}
+                    name='email'
+                    placeholder='type email'
+                    maxLength='30'
+                    type='email'
+                    />
+                </label>
+
+                <label>Role:&nbsp;
+                    <select value={values.role} onChange={onChange}name='role'>
+                        <option value="">-- Select a Role --</option>
+                        <option value="student">Student</option>
+                        <option value="tl">Team Lead</option>
+                        <option value="alumni" >Alumni</option>
+                        <option value="boss">Boss</option>
+                    </select>
+                </label>
+                <label>Single
+                    <input 
+                     type="radio"
+                     name='civil'
+                     value='single'
+                     checked={values.civil === 'single'}
+                     onChange={onChange}
+                    />
+                </label>
+                <label>Married
+                    <input
+                     type="radio"
+                     name="civil"
+                     value='married'
+                     checked={values.civil === 'married'}
+                     onChange={onChange}
+                    />
+                </label>
+                <div className='form-group checkboxes'>
+                    <h4>Hobbies</h4>
+                     <label>Hiking
+                      <input
+                      type="checkbox"
+                      name='hiking'
+                      checked={values.hobbies.hiking}
+                      onChange={onCheckboxChange}
+                    />
+                    </label>
+                    <label>Reading
+                    <input
+                      type="checkbox"
+                      name="reading"
+                      checked={values.hobbies.reading}
+                      onChange={onCheckboxChange}
+                    />
+                    </label>
+                    <label>Coding
+                    <input
+                      type="checkbox"
+                      name="coding"
+                      checked={values.hobbies.coding}
+                      onChange={onCheckboxChange}
+                    />
+                    </label>
+                </div>
+                <button 
+                 disabled={!values.username|| !values.email || !values.role ? true : false
+                }>Done</button>
+            
+            </div>
+
+        </form>
+    )
+}
+
+
+
